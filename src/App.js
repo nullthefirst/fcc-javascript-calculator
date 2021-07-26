e89324a0d7b9b6b246371efaed3c8b1b762d9e91
Ninte <ninte.dev@gmail.com>
2021-07-26 16:46:23 +0100

    Integrated freecodecamp test suite package.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Calculator from './calculator';
+import ReactFCCtest from 'react-fcctest';
 
 function App() {
   return <div id="app">
+    <ReactFCCtest />
     <Calculator />
   </div>;
 }
